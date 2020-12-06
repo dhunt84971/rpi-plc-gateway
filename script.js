230e3b587e74337a38fed3b0b47fd93dd1a0f63e
dhunt84971 <dhuntlinux@gmail.com>
2020-12-06 15:48:26 -0500

    Progress commit.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//#region GLOBAL DECLARATIONS
 "use strict";
+
+//#region REQUIRES EXTERNAL MODULES
 const electron = require("electron");
 const {
     remote
@@ -12,7 +13,13 @@ const exec = require("child_process").exec;
 const path = require('path');
 const os = require('os');
 const ping = require('ping');
+const libAppSettings = require("lib-app-settings");
+var piWifi = require('pi-wifi');
+//#endregion REQUIRES EXTERNAL MODULES
 
+//#region GLOBAL DECLARATIONS
+const settingsFile = ".settings";
+const appSettings = new libAppSettings(settingsFile);
 var quitApp = false;
 var plcIP = "100.100.100.50";
 var plcCheckTimeSet = 10;
@@ -21,9 +28,12 @@ var plcChecking = false;
 
 let appDir = path.dirname(require.main.filename);
 
+// Page component references.
 var lblWANIP = document.getElementById("lblWANIP");
 var lblLANIP = document.getElementById("lblLANIP");
 var lblPLCIP = document.getElementById("lblPLCIP");
+var txtSSID = document.getElementById("txtSSID");
+var lstSSIDs = document.getElementById("lstSSIDs");
 
 function plcCheckConn(){
     plcChecking = true;
@@ -96,15 +106,56 @@ function reboot(){
     });
 }
 
+function addItemtoDiv(divById, itemInnerText, classAdd) {
+    var newItem = document.createElement("div");
+    newItem.innerText = itemInnerText;
+    newItem.classList.add(classAdd);
+    document.getElementById(divById).appendChild(newItem);
+}
+
+function addSSID(ssid){
+    var newItem = document.createElement("div");
+    newItem.innerText = ssid;
+    newItem.classList.add("btn");
+    lstSSIDs.appendChild(newItem);
+    newItem.addEventListener("click", (e)=>{
+        selectSSID(e.target);
+    });
+}
+
+function loadSSIDs(){
+    piWifi.listNetworks(function(err, networksArray) {
+        if (err) {
+          return console.error(err.message);
+        }
+        for (let network of networksArray){
+            addSSID(network.ssid);
+        }
+      });
+}
+
+function selectSSID(el){
+    txtSSID.value = el.innerText;
+}
+
+
 async function init() {
     await wait(1000);
+    // Initialize the page components
     lblLANIP.innerText = "UNPLUGGED";
     lblLANIP.classList.add("invalid");
     lblWANIP.innerText = "UNPLUGGED";
     lblWANIP.classList.add("invalid");
     lblPLCIP.innerText = plcIP;
     lblPLCIP.classList.add("invalid");
+
+    // Load the SSID list.
+    loadSSIDs();
+
+    // Fill the page components
     getIPAddresses();
+
+    // Run the connected status loop.
     loop();
 }
 
@@ -129,6 +180,20 @@ function wait(time_ms) {
         }, time_ms);
     });
 }
+
+function ssid_clicked(e){
+    selectSSID(e.target);
+}
+
+document.getElementById("btnSettings").addEventListener("click", ()=>{
+    document.getElementById("divHomePage").classList.add("hide");
+    document.getElementById("divSettingsPage").classList.remove("hide");
+});
+
+document.getElementById("btnHome").addEventListener("click", ()=>{
+    document.getElementById("divHomePage").classList.remove("hide");
+    document.getElementById("divSettingsPage").classList.add("hide");
+});
 
 document.getElementById("btnReboot").addEventListener("click", () => {
     reboot();
